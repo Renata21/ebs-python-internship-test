963e8c3f069e9941572d7f23f43b9ba59bc653af
Renata21 <celac.renata@gmail.com>
2022-08-15 20:40:39 +0300

    update

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apps.blog.views import CategoryViewSet, BlogListView, BlogItemView
+from apps.blog.views import CategoryViewSet, BlogListView, BlogItemView, CreateBlogPostView, CreateCommentView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -11,4 +11,7 @@ urlpatterns = router.urls
 urlpatterns += [
     path('blog/', BlogListView.as_view(), name='blog_list'),
     path('blog/<int:pk>/', BlogItemView.as_view(), name='blog_item'),
+    path('blog/add', CreateBlogPostView.as_view(), name='add_blog_post'),
+    path('blog/comment', CreateCommentView.as_view(), name='add_comment'),
+
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,10 +1,13 @@
+from unicodedata import category
 from rest_framework import viewsets
 from rest_framework.generics import GenericAPIView, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
-from apps.blog.models import Category, Blog
-from apps.blog.serializers import CategorySerializer, BlogSerializer
+from apps.blog.models import Category, Blog, Comment
+from apps.blog.serializers import CategorySerializer, BlogSerializer, CommentSerializer
+
+from drf_util.decorators import serialize_decorator
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -25,12 +28,63 @@ class BlogListView(GenericAPIView):
 
 
 class BlogItemView(GenericAPIView):
-    serializer_class = BlogSerializer
+    serializer_class = BlogSerializer, CommentSerializer
 
     permission_classes = (AllowAny,)
     authentication_classes = ()
 
     def get(self, request, pk):
         blog = get_object_or_404(Blog.objects.filter(pk=pk))
+        comments = Comment.objects.all().filter(blog_id=pk)
+        param = {
+            "blog": BlogSerializer(blog).data,
+            "comments": CommentSerializer(comments, many=True).data
+        }
+        return Response(param)
+
+
+class CreateBlogPostView(GenericAPIView):
+    ''' 
+    Endpoint for creating a blog post that will add a new record in blog table
+    '''
+    serializer_class = BlogSerializer
+
+    permission_classes = (AllowAny,)
+    authentication_classes = ()
+
+    @serialize_decorator(BlogSerializer)
+    def post(self, request):
+        validated_data = request.serializer.validated_data
+
+        blog = Blog.objects.create(
+            title=validated_data['title'],
+            slug=validated_data['slug'],
+            body=validated_data['body'],
+            category=validated_data['category'],
+            enabled=True
+        )
+        blog.save()
 
         return Response(BlogSerializer(blog).data)
+
+
+class CreateCommentView(GenericAPIView):
+    ''' 
+    Endpoint for creating a comment to a blog post
+    '''
+    serializer_class = CommentSerializer
+
+    permission_classes = (AllowAny,)
+    authentication_classes = ()
+
+    @serialize_decorator(CommentSerializer)
+    def post(self, request):
+        validated_data = request.serializer.validated_data
+
+        comment = Comment.objects.create(
+            text = validated_data['text'],
+            blog = validated_data['blog']
+        )
+        comment.save()
+
+        return Response(CommentSerializer(comment).data)
